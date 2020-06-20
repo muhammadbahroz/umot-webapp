b0b4178d97e39fc1143b5b40d683092ed649a68e
muhammadbahroz <bahroz_ahmad@hotmail.com>
2020-06-20 16:35:52 +0500

    update

--- src/app/pages/home/faq/faq.component.ts
+++ src/app/pages/home/faq/faq.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FaqComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {}
+
+  search(search: string){
+    this.router.navigate(['home/menu-search', search]);
+  }
 
   ngOnInit() {}
 
