fec5b23e88437f03c5071c1226d9f9ffcfc76e9f
taurus-77 <saeedtaurus77@gmail.com>
2020-06-20 11:21:35 +0500

    added dynamic movie providers

--- src/app/pages/home/menu-search/menu-search.component.ts
+++ src/app/pages/home/menu-search/menu-search.component.ts
@@ -17,10 +17,28 @@ export class MenuSearchComponent implements OnInit {
   search(input: string){
     this.searchService.sendGETRequestWithParameters(input).subscribe((result: any) => {
       this.movies = JSON.parse(result.data);
+      this.movies = this.movies.slice(0, 10);
       this.movies.forEach(movie => {
+        const PROVIDERS_CHECK = [false, false, false, false];
         movie.img = 'https://image.tmdb.org/t/p/w500' + movie.img;
+        movie.providers.forEach(provider => {
+          if (provider.provider_name === 'HBO Now' || provider.provider_name === 'HBO Go'){
+            PROVIDERS_CHECK[0] = true;
+          }
+          if (provider.provider_name === 'Netflix' || provider.provider_name === 'Netflix Kids'){
+            PROVIDERS_CHECK[1] = true;
+          }
+          if (provider.provider_name === 'Amazon Video' || provider.provider_name === 'Amazon Prime Video'){
+            PROVIDERS_CHECK[2] = true;
+          }
+          if (provider.provider_name === 'Apple iTunes' || provider.provider_name === 'Apple TV Plus'){
+            PROVIDERS_CHECK[3] = true;
+          }
+        });
+        movie.providers = PROVIDERS_CHECK;
       });
       console.log(this.movies);
+
     });
   }
 
