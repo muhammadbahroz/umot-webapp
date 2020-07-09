bde97744db0962406cc54473ed00494f7db88879
muhammadbahroz <bahroz_ahmad@hotmail.com>
2020-07-10 00:11:13 +0500

    search added on movie page

--- src/app/pages/home/movie/movie.component.ts
+++ src/app/pages/home/movie/movie.component.ts
@@ -11,6 +11,7 @@ export class MovieComponent implements OnInit {
 
   searchValue: string;
   movieID: string;
+  check:boolean = false;
   constructor(private router: Router , private searchService: SearchService, private route: ActivatedRoute) {
       this.movieID = this.route.snapshot.paramMap.get('id');
       this.movieClick();
@@ -21,6 +22,39 @@ export class MovieComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+
+  movies: any;
+
+  search(){
+    this.searchService.search(this.searchValue).subscribe((result: any) => {
+      this.movies = JSON.parse(result.data);
+      this.movies = this.movies.slice(0, 2);
+      this.movies.forEach(movie => {
+        movie.img = 'https://image.tmdb.org/t/p/w500' + movie.img;
+      });
+      if(this.searchValue !=  "")
+      {
+        this.check=true;
+      }
+      else{
+        this.check=false;
+      }
+
+      // console.log(this.movies);  
+    });
+  }
+
+  openMovieClicked(movieID: string){
+
+    this.router.navigate(['home/movie', movieID]);
+
+    // this.searchService.movie(movieID).subscribe((result: any) => {
+    //   this.movieDetail = JSON.parse(result.data);
+    //   // this.movies = this.movies.slice(0, 10);
+    //   console.log(this.movieDetail);
+    // });
   }
 
   movieDetail: any = null;
